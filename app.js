be1cd31bc307d1c3a0c8c9da6859d4dfd75b2701
ytakaya <mt.tky@icloud.com>
2020-01-18 14:45:12 +0900

    メッセージの受信を確認

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+require('dotenv').config()
 const express = require('express');
 const path = require('path');
 const PORT = 5000;
+const line = require('@line/bot-sdk');
+const config = {
+  channelAccessToken: process.env.ACCESS_TOKEN,
+  channelSecret: process.env.SECRET_KEY
+};
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -9,5 +15,10 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/g/', (req, res) => res.json({method: "こんにちは、getさん"}))
   .post('/p/', (req, res) => res.json({method: "こんにちは、postさん"}))
-  .post('/hook/', (req, res) => res.json({test: "hook"}))
+  .post('/hook/', line.middleware(config) ,(req, res) => lineBot(req, res))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+  function lineBot(req, res) {
+    console.log(req.body.events);
+    res.status(200).end();
+  }
