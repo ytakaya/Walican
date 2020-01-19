c505f766ff87ca3d076aef9b98af8027619c7afd
ytakaya <mt.tky@icloud.com>
2020-01-19 17:01:46 +0900

    返信する

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const config = {
   channelAccessToken: process.env.ACCESS_TOKEN,
   channelSecret: process.env.SECRET_KEY
 };
+const client = new line.Client(config);
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -14,11 +15,27 @@ express()
   .set('view engin', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/g/', (req, res) => res.json({method: "こんにちは、getさん"}))
-  .post('/p/', (req, res) => res.json({method: "こんにちは、postさん"}))
+  .post('/', (req, res) => res.status(200).end())
   .post('/hook/', line.middleware(config) ,(req, res) => lineBot(req, res))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
   function lineBot(req, res) {
-    console.log(req.body.events);
     res.status(200).end();
+    const events = req.body.events;
+    const promises = [];
+    for (let i = 0, l = events.length; i < l; i++) {
+      const ev = events[i];
+      promises.push(
+        echoman(ev)
+      );
+    }
+    Promise.all(promises).then(console.log("pass"));
+  }
+
+  async function echoman(ev) {
+    const pro = await client.getProfile(ev.source.userId);
+    return client.replyMessage(ev.replyToken, {
+      type: "text",
+      text: `${pro.displayName}さん、今「${ev.message.text}」って言いました？`
+    })
   }
