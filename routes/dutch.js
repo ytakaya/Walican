a97380930ddf682f9c4f6ebaf94d58e024997f60
ytakaya <mt.tky@icloud.com>
2020-01-20 12:33:21 +0900

    ユーザーIDからユーザー情報の取得

--- routes/dutch.js
+++ routes/dutch.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const MongoClient = require("mongodb").MongoClient;
 const url = require('url');
 
-function getUsersByPayId(pay_id) {
+function getUserIdByPayId(pay_id) {
   return new Promise(resolve => {
     MongoClient.connect(CONNECTION_URL, OPTIONS, (error, client) => {
       const db = client.db(DATABASE);
@@ -24,15 +24,44 @@ function getUsersByPayId(pay_id) {
   })
 }
 
+function getUserByUserId(user_id) {
+  return new Promise(resolve => {
+    MongoClient.connect(CONNECTION_URL, OPTIONS, (error, client) => {
+      const db = client.db(DATABASE);
+      db.collection("users").findOne({
+        user_id: user_id
+      }).then((user) => {
+        const user_info = {
+          id: user.user_id,
+          name: user.user_name,
+          img: user.img,
+        }
+        resolve(user_info);
+      })
+    })
+  })
+}
+
+const getUsersByUserIds = async (user_ids) => {
+  const promises = [];
+  user_ids.forEach(user_id => {
+    promises.push(getUserByUserId(user_id));
+  })
+  return Promise.all(promises);
+}
+
 
 router.get("/", (req, res) => {
   const payId = url.parse(req.url, true).query.payId;
-  getUsersByPayId(payId).then((users) => {
-    const doc = {
-      payId: payId,
-      users: users,
-    }
-    res.render("../views/dutch.ejs", doc);
+  getUserIdByPayId(payId).then((user_ids) => {
+    getUsersByUserIds(user_ids).then((users) => {
+      console.log(users);
+    })
+    // const doc = {
+    //   payId: payId,
+    //   users: users,
+    // }
+    // res.render("../views/dutch.ejs", doc);
   })
 });
 
