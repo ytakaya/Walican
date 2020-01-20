d270b6258a33ddce9d9c1f250f486aa03b60f419
ytakaya <mt.tky@icloud.com>
2020-01-20 11:14:03 +0900

    paymentIdをDBに登録

--- src/pay/index.js
+++ src/pay/index.js
@@ -1,10 +1,37 @@
+const {CONNECTION_URL, DATABASE, OPTIONS} = require("../../config/mongodb.config");
+const MongoClient = require("mongodb").MongoClient;
 const generateQuestionButton = require('../utils/generateQuestionButton');
-const questionButton = generateQuestionButton();
+
+const insertPayments = function(parent_user, group_id, payment_id) {
+  MongoClient.connect(CONNECTION_URL, OPTIONS, (error, client) => {
+    const db = client.db(DATABASE);
+    db.collection("payments").insertOne(
+      {
+        payments_id: payment_id,
+        group_id: group_id,
+        parent: parent_user,
+        status: "pending",
+      }
+    ).catch(() => {
+      console.log(error);
+    }).then(() => {
+      client.close();
+    });
+  }); 
+};
 
 exports.payBubble = function(client, ev) {
-  return client.replyMessage(ev.replyToken, {
-    type: "flex",
-    altText: "Walicanからのメッセージ",
-    contents: questionButton,
+  generateQuestionButton().then(res => {
+    const {paymentId, questionButton} = res;
+    // const {userId, groupId} = ev.source;
+    const userId = 'sample_user_1';
+    const groupId = 'sample_group_1';
+    insertPayments(userId, groupId, paymentId);
+    
+    return client.replyMessage(ev.replyToken, {
+      type: "flex",
+      altText: "Walicanからのメッセージ",
+      contents: questionButton,
+    })
   })
 }
--- src/utils/generateQuestionButton.js
+++ src/utils/generateQuestionButton.js
@@ -1,68 +1,71 @@
 const uuid = require('uuid/v1');
 
-function generateQuestionButton () {
+const generateQuestionButton = async () => {
   const paymentId = uuid();
 
   return {
-    "type": "bubble",
-    "body": {
-      "type": "box",
-      "layout": "vertical",
-      "spacing": "md",
-      "contents": [
-        {
-          "type": "text",
-          "text": "選択してピエ"
-        },
-        {
-          "type": "separator"
-        },
-        {
-          "type": "text",
-          "text": `payId: ${paymentId}`
-        },
-        {
-          "type": "button",
-          "style": "primary",
-          "action": {
-            "type": "uri",
-            "label": "割り勘",
-            "uri": `https://c06164a6.ngrok.io/dutch?payId=${paymentId}`
+    paymentId: paymentId,
+    questionButton: {
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "md",
+        "contents": [
+          {
+            "type": "text",
+            "text": "選択してピエ"
+          },
+          {
+            "type": "separator"
+          },
+          {
+            "type": "text",
+            "text": `payId: ${paymentId}`
+          },
+          {
+            "type": "button",
+            "style": "primary",
+            "action": {
+              "type": "uri",
+              "label": "割り勘",
+              "uri": `https://c06164a6.ngrok.io/dutch?payId=${paymentId}`
+            }
+          },
+          {
+            "type": "button",
+            "style": "primary",
+            "action": {
+              "type": "uri",
+              "label": "貸した",
+              "uri": `https://c06164a6.ngrok.io/borrow/?payId=${paymentId}`
+            }
+          },
+          {
+            "type": "separator"
+          },
+          {
+            "type": "button",
+            "style": "secondary",
+            "action": {
+              "type": "uri",
+              "label": "認証状況",
+              "uri": `https://c06164a6.ngrok.io/auth_status/?payId=${paymentId}`
+            }
+          },
+          {
+            "type": "button",
+            "style": "secondary",
+            "action": {
+              "type": "uri",
+              "label": "キャンセル",
+              "uri": `https://c06164a6.ngrok.io/cancel/?payId=${paymentId}`
+            }
           }
-        },
-        {
-          "type": "button",
-          "style": "primary",
-          "action": {
-            "type": "uri",
-            "label": "貸した",
-            "uri": `https://c06164a6.ngrok.io/borrow/?payId=${paymentId}`
-          }
-        },
-        {
-          "type": "separator"
-        },
-        {
-          "type": "button",
-          "style": "secondary",
-          "action": {
-            "type": "uri",
-            "label": "認証状況",
-            "uri": `https://c06164a6.ngrok.io/auth_status/?payId=${paymentId}`
-          }
-        },
-        {
-          "type": "button",
-          "style": "secondary",
-          "action": {
-            "type": "uri",
-            "label": "キャンセル",
-            "uri": `https://c06164a6.ngrok.io/cancel/?payId=${paymentId}`
-          }
-        }
-      ]
+        ]
+      }
     }
-  };
+  }
 }
 
 exports = module.exports = generateQuestionButton;
