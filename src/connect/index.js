ef6cada632bc22f3debc00baeb525938c1b9b8db
ytakaya <mt.tky@icloud.com>
2020-01-22 07:53:31 +0900

    add status

--- src/connect/index.js
+++ src/connect/index.js
@@ -1,6 +1,23 @@
-exports.connectUser = function(client, ev) {
+exports.connectUser = async (evsource, user_profile, client, ev) => {
+  const status = await registGroupAndUser(evsource, user_profile);
+  
+  let text;
+  if (status == 'alreadyRegisted') {
+    text = `「${user_profile.displayName}」は登録済みだよ！`
+  } else if (status == 'success') {
+    text = `「${user_profile.displayName}」を登録したよ！`
+  } else {
+    text = `「${user_profile.displayName}」を登録に失敗したよ...`
+  }
+
   return client.replyMessage(ev.replyToken, {
     type: "text",
-    text: "使ってくれてありがとうピエ\n使い方\n/attend: ユーザーを登録\n/payment: 割り勘登録\n/auth: 割り勘を認証"
+    text: text
+  })
+}
+
+function registGroupAndUser(evsource, user_profile) {
+  return new Promise(resolve => {
+
   })
 }
