d55e5a71b66d2e7fa7cc67934ba0f878aae96495
ytakaya <mt.tky@icloud.com>
2020-01-23 21:59:11 +0900

    ユーザー登録

--- src/connect/index.js
+++ src/connect/index.js
@@ -7,7 +7,7 @@ exports.connectUser = async (evsource, user_profile, client, ev) => {
   let text;
   if (status == 'alreadyRegisted') {
     text = `「${user_profile.displayName}」は登録済みだよ！`
-  } else if (status == 'success') {
+  } else if (status) {
     text = `「${user_profile.displayName}」を登録したよ！`
   } else {
     text = `「${user_profile.displayName}」を登録に失敗したよ...`
@@ -25,40 +25,17 @@ function registGroupAndUser(evsource, user_profile) {
       const db = client.db(DATABASE);
       Promise.all([
         findAndInsertGroups(db, evsource.groupId, evsource.userId),
+        findAndInsertUsers(db, evsource.userId, user_profile),
       ]).catch(() => {
         console.log(error);
-      }).then(() => {
+        resolve(false);
+      }).then((v) => {
+        resolve(v[0]);
         client.close();
       });
     });
-    resolve("ok")
   })
 }
-
-// router.post("/*", (req, res) => {
-//   MongoClient.connect(CONNECTION_URL, OPTIONS, (error, client) => {
-//     const db = client.db(DATABASE);
-//     db.collection("rooms").findOne({
-//       room_url: "/sample.html"
-//     }).then((room) => {
-//       db.collection("messages").find({
-//         room_url: room.room_url,
-//       }).toArray((err, messages) => {
-//         if (err) throw err;
-//         const doc = {
-//           room: room,
-//           messages: messages,
-//           user_name: req.body.user_name,
-//         };
-//         res.render("./rooms/index.ejs", doc);
-//       });
-//     }).catch((error) => {
-//       throw error;
-//     }).then(() => {
-//       client.close();
-//     });
-//   });
-// });
 
 const findAndInsertGroups = function(db, group_id, user_id) {
   return new Promise(resolve => {
@@ -66,15 +43,21 @@ const findAndInsertGroups = function(db, group_id, user_id) {
       group_id: group_id
     }).then((group) => {
       if (group != null) {
-        group.users.push(user_id);
-        db.collection("groups").updateOne({
-          group_id: group_id
-        }, {
-          $set: group
-        }).then(() => {
-          console.log("insert user to group");
-          resolve(true);
-        })
+        if (group.users.indexOf(user_id) != -1) {
+          console.log("already inserted to group")
+          resolve("alreadyRegisted");
+        }
+        else {
+          group.users.push(user_id);
+          db.collection("groups").updateOne({
+            group_id: group_id
+          }, {
+            $set: group
+          }).then(() => {
+            console.log("insert user to group");
+            resolve(true);
+          })
+        }
       }
       else {
         db.collection("groups").insertOne(
@@ -91,27 +74,27 @@ const findAndInsertGroups = function(db, group_id, user_id) {
   })
 };
 
-// const findAndInsertUsers = function(db, user_profile) {
-//   return Promise.all([
-//     db.collection("users").insertMany([
-//       {
-//         user_id: "sample_user_1",
-//         user_name: "adam",
-//         img: "/images/sample_img.png",
-//         attend_date: new Date(2020, 1, 10),
-//       },
-//       {
-//         user_id: "sample_user_2",
-//         user_name: "bob",
-//         img: "/images/sample_img.png",
-//         attend_date: new Date(2020, 1, 10),
-//       },
-//       {
-//         user_id: "sample_user_3",
-//         user_name: "eve",
-//         img: "/images/sample_img.png",
-//         attend_date: new Date(2020, 1, 10),
-//       },
-//     ]),
-//     db.collection("users").createIndex({ user_id: 1 }, { unique: true, background: true })
-//   ]);
+const findAndInsertUsers = function(db, user_id, user_profile) {
+  return new Promise(resolve => {
+    db.collection("users").findOne({
+      user_id: user_id
+    }).then((user) => {
+      if (user != null) {
+        console.log("user already registed")
+        resolve(true);
+      }
+      else {
+        db.collection("users").insertOne(
+          {
+            user_id: user_id,
+            user_name: user_profile.displayName,
+            img: user_profile.pictureUrl,
+          }
+        ).then(() => {
+          console.log("create user");
+          resolve(true);
+        })
+      }
+    })
+  })
+}
