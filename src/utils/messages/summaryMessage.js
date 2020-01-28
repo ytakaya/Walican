591a13762ac752a7602d8168a642c1de97529ac3
ytakaya <mt.tky@icloud.com>
2020-01-28 23:27:18 +0900

    use span

--- src/utils/messages/summaryMessage.js
+++ src/utils/messages/summaryMessage.js
@@ -10,15 +10,52 @@ const summaryMessage = async (summary, user_info) => {
     Object.keys(summary[userA_id]).forEach(userB_id => {
       if (userB_id == userA_id) return;
       const amount = summary[userA_id][userB_id];
+      const color = (amount < 0) ? '#ff0088' : '#0077ff';
+      // contents.push({
+      //   "type": "box",
+      //   "layout": "horizontal",
+      //   "spacing": "md",
+      //   "contents": [
+      //     {
+      //       "type": "span",
+      //       "text": `${user_info[userB_id]}:`
+      //     },
+      //     {
+      //       "type": "span",
+      //       "text": amount
+      //     }
+      //   ]
+      // })
       contents.push({
-        "type": "text",
-        "text": `${user_info[userB_id]}: ${amount}`
-      })
+          "type": "box",
+          "layout": "horizontal",
+          "contents": [
+            {
+              "type": "text",
+              "contents": [
+                {
+                  "type": "span",
+                  "text": "hello, world"
+                }
+              ]
+            },
+            {
+              "type": "text",
+              "contents": [
+                {
+                  "type": "span",
+                  "text": "hello, world"
+                }
+              ]
+            }
+          ]
+        }
+      )
     })
 
     contents.push({"type": "text", "text": "\n"});
   })
-
+  
   return {
     "type": "bubble",
     "body": {
