5c998f4c8fd4aa3d401d5ec4338af0160fa28fba
ytakaya <mt.tky@icloud.com>
2020-02-11 21:20:20 +0900

    fix

--- src/utils/messages/authMessage.js
+++ src/utils/messages/authMessage.js
@@ -1,5 +1,4 @@
 const authMessage = async (amount, propose, paymentId, user_names, parent) => {
-  console.log(parent)
   const names = user_names.join(", ");
   const propose_message = (propose=='') ? 'なし' : propose;
   return {authInfo: `${names}さん、${amount}円の認証依頼が来ました。以下のメッセージをコピーして送信してね\n目的「${propose_message}」`, authMessage: `/auth ${paymentId}`}
