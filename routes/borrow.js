d3b1a6ac6a1df73b9b802e911982c461d31bfe8b
ytakaya <mt.tky@icloud.com>
2020-02-17 13:12:15 +0900

    update borrow auth message

--- routes/borrow.js
+++ routes/borrow.js
@@ -3,7 +3,7 @@ const url = require('url');
 
 const pay = require('../src/pay/index');
 const db_logics = require('../src/utils/dbs/logics');
-
+const rate = require('../src/utils/rate/index');
 
 router.get("/", (req, res) => {
   const payId = url.parse(req.url, true).query.payId;
@@ -33,16 +33,26 @@ router.post("/regist", (req, res) => {
   const target_user = req.body.target_user;
   let children = {};
   children[target_user] = false;
-  db_logics.updatePayments(req.body.payId, {children: children, method: 'borrow', amount: req.body.amount, status: "auth_pending"});
-  db_logics.getGroupIdAndParentByPayId(req.body.payId).then((response) => {
-    db_logics.getUsersByUserIds([target_user]).then((users) => {
-      const user_names = [];
-      users.forEach(user => {
-        user_names.push(user.name);
+
+  rate.currencyConvert(req.body.amount, req.body.currency).then(converted_amount => {
+    const jpy = Math.round(converted_amount);
+    const data = {
+      currency: req.body.currency,
+      original: req.body.amount,
+      jpy: jpy
+    };
+
+    db_logics.updatePayments(req.body.payId, {children: children, method: 'borrow', amount: jpy, status: "auth_pending"});
+    db_logics.getGroupIdAndParentByPayId(req.body.payId).then((response) => {
+      db_logics.getUsersByUserIds([target_user]).then((users) => {
+        const user_names = [];
+        users.forEach(user => {
+          user_names.push(user.name);
+        })
+        pay.authBubble(req.body.payId, data, req.body.propose, response.group_id, user_names, response.parent, 'borrow');
+        console.log("ok")
+        res.redirect("/complete/success");
       })
-      pay.authBubble(req.body.payId, req.body.amount, req.body.propose, response.group_id, user_names, response.parent, 'borrow');
-      console.log("ok")
-      res.redirect("/complete/success");
     })
   })
 })
