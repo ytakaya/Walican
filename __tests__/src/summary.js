70c651a61ecb96720bbdf204ce04db2e9014af6b
ytakaya <mt.tky@icloud.com>
2020-02-19 22:38:47 +0900

    add testcase2

--- __tests__/src/summary.js
+++ __tests__/src/summary.js
@@ -3,12 +3,14 @@ const target = require('../../src/summary/index');
 describe('summary test', () => {
   describe('func _payoff', () => {
     test('adam borrow to bob', () => {
-      const datas = [{ 
-        parent: 'adam',
-        method: 'borrow',
-        amount: 1000,
-        children: ['bob'] 
-      }]
+      const datas = [
+        { 
+          parent: 'adam',
+          method: 'borrow',
+          amount: 1000,
+          children: ['bob'] 
+        }
+      ]
 
       const users = ['adam', 'bob']
 
@@ -20,6 +22,39 @@ describe('summary test', () => {
           },
           bob: {
             adam: -1000,
+            bob: 0
+          }
+        }
+        expect(summary).toEqual(ans)
+      })
+    })
+
+    test('adam borrow to bob and bob borrow to adam', () => {
+      const datas = [
+        { 
+          parent: 'adam',
+          method: 'borrow',
+          amount: 1000,
+          children: ['bob'] 
+        },
+        {
+          parent: 'bob',
+          method: 'borrow',
+          amount: 1000,
+          children: ['adam']
+        }
+      ]
+
+      const users = ['adam', 'bob']
+
+      target.payoff(datas, users).then(summary => {
+        const ans = {
+          adam: {
+            adam: 0,
+            bob: 0
+          },
+          bob: {
+            adam: 0,
             bob: 0
           }
         }
