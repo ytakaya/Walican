a3ae33f050ba996ebc9f7561a0381a0380371ff8
ytakaya <mt.tky@icloud.com>
2020-02-20 21:26:48 +0900

    update app.js

--- app.js
+++ app.js
@@ -3,20 +3,12 @@ const express = require('express');
 const app = express();
 const http = require("http").Server(app);
 const bodyParser = require("body-parser");
-const PORT = 5000;
-const line = require('@line/bot-sdk');
-const config = {
-  channelAccessToken: process.env.ACCESS_TOKEN,
-  channelSecret: process.env.SECRET_KEY
-};
-const client = new line.Client(config);
-const lineBot = require('./routes/hook');
+const PORT = 8080;
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-// app.use("/borrow/", require("./routes/borrow.js"));
 app.use("/hook", require("./routes/hook.js"));
 
 app.use(bodyParser.urlencoded({extended: true}));
