4407fe95056d979d0002ba86c87bae338420b60c
ytakaya <mt.tky@icloud.com>
2020-02-27 21:58:51 +0900

    session regenerate

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
+const { SESSION_SECRET } = require("./config/app.config").security;
 const express = require('express');
 const app = express();
 const http = require("http").Server(app);
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 const flash = require("connect-flash");
 const accountcontrol = require("./lib/security/accountcontrol");
 const PORT = 8080;
@@ -12,6 +15,14 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.use("/hook", require("./routes/hook.js"));
+
+app.use(cookieParser());
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  name: "sid"
+}));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
---
+++ config/app.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  security: {
+    SESSION_SECRET: "***session_secret***"
+  } 
+}
