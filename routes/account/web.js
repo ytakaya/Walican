16639ea9ad4f79887a109daa6eee169c8048a5e1
ytakaya <mt.tky@icloud.com>
2020-03-01 23:24:28 +0900

    add user page

--- routes/account/web.js
+++ routes/account/web.js
@@ -3,7 +3,12 @@ const url = require('url');
 const { authorize } = require("../../lib/security/accountcontrol");
 
 router.get("/user", authorize(), (req, res) => {
-  console.log(req.user)
+  const docs = {
+    userName: req.user.name,
+    userId: req.user.id,
+    userImg: req.user.img
+  }
+  res.render('./account/web/user-page.ejs', docs)
 });
 
 module.exports = router;
