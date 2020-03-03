aaa6376d27092d011fa114daf1ee2d01bd68778a
ytakaya <mt.tky@icloud.com>
2020-03-03 19:51:09 +0900

    update header and userpage

--- routes/account/web.js
+++ routes/account/web.js
@@ -4,11 +4,7 @@ const { authorize } = require("../../lib/security/accountcontrol");
 const db_logics = require('../..//src/utils/dbs/logics');
 
 router.get("/user", authorize(), (req, res) => {
-  const docs = {
-    userName: req.user.name,
-    userId: req.user.id,
-    userImg: req.user.img
-  }
+  const docs = {};
   db_logics.getGroupsByuserId(req.user.id).then(groups => {
     docs.groups = groups;
     res.render('./account/web/user-page.ejs', docs)
