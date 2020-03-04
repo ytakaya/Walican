646f85631296d090941c655302350412e4922903
ytakaya <mt.tky@icloud.com>
2020-03-04 16:46:22 +0900

    update payments operation(dutch)

--- routes/account/oauth.js
+++ routes/account/oauth.js
@@ -8,7 +8,7 @@ const { authenticate } = require("../../lib/security/accountcontrol");
 const CHANNEL_ID = process.env.CHANNEL_ID;
 const CHANNEL_SECRET = process.env.CHANNEL_SECRET;
 const Callback_URL = `${process.env.HOST_URL}/oauth/getToken`;
-const State = 'falehigiao3';
+const State = Math.random().toString(32).substring(2);
 const oauth_url = `https://access.line.me/dialog/oauth/weblogin?response_type=code&client_id=${CHANNEL_ID}&redirect_uri=${Callback_URL}&state=${State}`
 
 
@@ -34,6 +34,7 @@ router.get("/getToken", (req1, res1) => {
     json: true
   };
   request.post(q1, (err, req2, res2) => {
+    console.log(res2.access_token)
     if (err) console.log(err);
     else {
       const q2 = {
@@ -44,6 +45,7 @@ router.get("/getToken", (req1, res1) => {
         if (err) console.log(err);
         else {
           const docs = JSON.parse(res3)
+          console.log(docs)
           res1.render('./account/oauth/login-confirm.ejs', docs);
         }
       })
--- routes/dutch.js
+++ routes/dutch.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const url = require('url');
+const uuid = require('uuid/v1');
 
 const pay = require('../src/pay/index');
 const db_logics = require('../src/utils/dbs/logics');
@@ -7,62 +8,89 @@ const rate = require('../src/utils/rate/index');
 
 
 router.get("/", (req, res) => {
-  const payId = url.parse(req.url, true).query.payId;
-  db_logics.getUserIdByPayId(payId).then((payment) => {
-    const {parent, user_ids, payment_status} = payment;
-    if (payment_status === 'pending') {
-      db_logics.getUsersByUserIds(user_ids).then((users) => {
-        const doc = {
-          payId: payId,
-          users: users,
-          parent: parent,
-        }
-        res.render("./dutch.ejs", doc);
-      })
-    }
-    else if (payment_status === 'canceled') {
-      res.redirect("/complete/canceledAuth");
-    }
-    else {
-      res.redirect('/complete/alreadySendAuth');
-    }
-  })
+  const groupId = url.parse(req.url, true).query.groupId;
+  if (!url.parse(req.url, true).query.payId) {
+    const parent = req.user.id;
+    const payId = uuid();
+    db_logics.getUsersByGroupId(groupId).then(users => {
+      const doc = {
+        payId: payId,
+        groupId: groupId,
+        users: users,
+        parent: parent,
+      }
+      res.render("./dutch.ejs", doc);
+    })
+  }
+  else {
+    const payId = url.parse(req.url, true).query.payId;
+    db_logics.getUserIdByPayId(payId).then((payment) => {
+      const {parent, user_ids, payment_status} = payment;
+      if (payment_status === 'pending') {
+        db_logics.getUsersByUserIds(user_ids).then((users) => {
+          const doc = {
+            payId: payId,
+            groupId: groupId,
+            users: users,
+            parent: parent,
+          }
+          res.render("./dutch.ejs", doc);
+        })
+      }
+      else if (payment_status === 'canceled') {
+        res.redirect("/complete/canceledAuth");
+      }
+      else {
+        res.redirect('/complete/alreadySendAuth');
+      }
+    })
+  }
 });
 
 router.post("/regist", (req, res) => {
   //エラーのリダイレクト処理いれる
-  db_logics.getGroupIdAndParentByPayId(req.body.payId).then((response) => {
-    const children = {}
-    req.body.target_users.forEach(target_user => {
-      if (target_user === response.parent)
-        children[target_user] = true;
-      else
-        children[target_user] = false;
+  const group_id = req.body.groupId;
+  const parent = req.body.parent;
+
+  const children = {}
+  req.body.target_users.forEach(target_user => {
+    if (target_user === parent)
+      children[target_user] = true;
+    else
+      children[target_user] = false;
+  })
+
+  rate.currencyConvert(req.body.amount, req.body.currency).then(converted_amount => {
+    const jpy = Math.round(converted_amount);
+    const data = {
+      currency: req.body.currency,
+      original: req.body.amount,
+      jpy: jpy
+    };
+
+    const query = {
+      payments_id: req.body.payId,
+      group_id: group_id,
+      parent: parent,
+      children: children, 
+      method: 'borrow', 
+      amount: jpy, 
+      status: "auth_pending"
+    }
+    db_logics.updatePayments(query);
+
+    const user_ids = [];
+    Object.keys(children).forEach(id => {
+      if (id != parent) user_ids.push(id);
     })
-
-    rate.currencyConvert(req.body.amount, req.body.currency).then(converted_amount => {
-      const jpy = Math.round(converted_amount);
-      const data = {
-        currency: req.body.currency,
-        original: req.body.amount,
-        jpy: jpy
-      };
-
-      db_logics.updatePayments(req.body.payId, {children: children, method: 'dutch', amount: jpy, status: "auth_pending"});
-
-      const user_ids = [];
-      Object.keys(children).forEach(id => {
-        if (id != response.parent) user_ids.push(id);
+    db_logics.getUsersByUserIds(user_ids).then((users) => {
+      const user_names = [];
+      users.forEach(user => {
+        user_names.push(user.name);
       })
-      db_logics.getUsersByUserIds(user_ids).then((users) => {
-        const user_names = [];
-        users.forEach(user => {
-          user_names.push(user.name);
-        })
-        pay.authBubble(req.body.payId, data, req.body.propose, response.group_id, user_names, response.parent, 'dutch');
-        console.log("ok")
-        res.redirect("/complete/success");
-      })
+      pay.authBubble(req.body.payId, data, req.body.propose, group_id, user_names, parent, 'dutch');
+      console.log("ok")
+      res.redirect("/complete/success");
     })
   })
 })
