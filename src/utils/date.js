4e1749748cf45d512c61a776caf4ee6a03b3902f
ytakaya <mt.tky@icloud.com>
2020-03-13 09:58:04 +0900

    fix date

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,12 +1,11 @@
 exports.getDate = () => {
   const date = new Date();
-  let format = 'YYYY-MM-DD HH:MM:SS'
+  let format = 'YYYY-MM-DD HH:MM'
   format = format.replace(/YYYY/, date.getFullYear());
   format = format.replace(/MM/, date.getMonth() + 1);
   format = format.replace(/DD/, date.getDate());
   format = format.replace(/HH/, date.getHours());
   format = format.replace(/MM/, date.getMinutes());
-  format = format.replace(/SS/, date.getSeconds());
 
   return format;
 }
